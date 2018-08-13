56b8da1ce98c54c90bb5f7f4f0d661e773f61a53
Marcin Czekala <marcin_czekala@icloud.com>
2018-08-14 00:51:15 +0200

    add

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '504862487460-s3cscgfk19sk7vinapmg3bchqhdv40fp.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'tMjR6spFr1nfPMBJrmNyaGeQ',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
